8e58b955f83923f9cc28a0d75c87828b7f07939a
Santhosh Kumar J <santhujayanna459@gmail.com>
2025-11-04 19:52:05 +0530

    added divisible_by_both program

---
+++ divisible_by_both.js
@@ -0,0 +1,18 @@
+let num = 15;
+ 
+if (num<0){
+  console.log("Enter positive number")
+}
+
+  else if (num%3 ==0 && num%5==0){
+    console.log("Divisible by both 3 and 5");
+  }
+  else if (num%3 == 0){
+    console.log("Divisible by only 3");
+  }
+  else if (num%5 ==0){
+    console.log("Divisible by only 5")
+  }
+  else {
+  console.log ("num is not divisible by both 3 and 5");
+  }

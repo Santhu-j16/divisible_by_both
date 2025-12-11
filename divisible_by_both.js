109bcdd58582efe16434f818968dcea86f413183
Santhosh Kumar J <santhujayanna459@gmail.com>
2025-12-11 12:16:22 +0530

    Fix spacing in condition for divisibility check

--- divisible_by_both.js
+++ divisible_by_both.js
@@ -4,7 +4,7 @@ if (num<0){
   console.log("Enter positive number")
 }
 
-  else if (num%3 ==0 && num%5==0){
+  else if (num%3 == 0 && num%5== 0){
     console.log("Divisible by both 3 and 5");
   }
   else if (num%3 == 0){

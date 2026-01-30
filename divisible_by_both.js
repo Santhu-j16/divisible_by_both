38946faacec16722c2e930c6bb28de51761553b2
Santhosh Kumar J <santhujayanna459@gmail.com>
2026-01-30 20:14:28 +0530

    Update divisible_by_both.js

--- divisible_by_both.js
+++ divisible_by_both.js
@@ -5,7 +5,7 @@ if (num<0){
 }
 
   else if (num%3 == 0 && num%5== 0){
-    console.log("Divisible by both 3 and 5");
+    console.log("Divisible by both 3 and 5"); 
   }
   else if (num%3 == 0){
     console.log ("Divisible by only 3");

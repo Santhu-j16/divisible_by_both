401e267318667e6d8ae30eb8de93e7d040040834
Santhosh Kumar J <santhujayanna459@gmail.com>
2026-02-10 20:06:38 +0530

    Fix conditional logic for divisibility checks

--- divisible_by_both.js
+++ divisible_by_both.js
@@ -6,7 +6,7 @@ if (num<0){
 
   else if (num%3 == 0 && num%5== 0){
     console.log("Divisible by both 3 and 5"); 
-  }
+  } 
   else if (num%3 == 0){
     console.log ("Divisible by only 3"); 
   }

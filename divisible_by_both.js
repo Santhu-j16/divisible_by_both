ba4477d0f7d598cfe76e8ac8a2ca079d13585599
Santhosh Kumar J <santhujayanna459@gmail.com>
2026-02-22 19:42:08 +0530

    Fix conditional check for positive number input

--- divisible_by_both.js
+++ divisible_by_both.js
@@ -1,9 +1,9 @@
 let num =10;
   
 if (num<0){
-  console.log("Enter positive number")
+  console.log("Enter positive number") 
 }
-
+ 
   else if (num%3 == 0 && num%5== 0){
     console.log("Divisible by both 3 and 5"); 
   } 

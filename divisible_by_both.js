838fc06255f8f8957d9b8c067dcc3d702e5f9ca4
Santhosh Kumar J <santhujayanna459@gmail.com>
2026-02-26 09:39:14 +0530

    Fix console log for divisibility by only 5

--- divisible_by_both.js
+++ divisible_by_both.js
@@ -11,7 +11,7 @@ if (num<0){
     console.log ("Divisible by only 3");  
   }
   else if (num%5 ==0){ 
-    console.log("Divisible by only 5")
+    console.log("Divisible by only 5") 
   }
   else {
   console.log ("num is not divisible by both 3 and 5");
